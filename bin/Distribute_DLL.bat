0c2ebaca4b5395e8819be06f3c6ca4364ac92678
alchemistmatt <matthew.monroe@pnnl.gov>
2018-06-25 17:17:05 -0700

    Update NuGet API key and distribution batch file

--- bin/Distribute_DLL.bat
+++ bin/Distribute_DLL.bat
@@ -1,15 +1,15 @@
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDPicker_PlugIn\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InspectResultsAssembly_PlugIn\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSGFDB_PlugIn\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\bin\DMS_Programs\AnalysisToolManager1\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDPicker_PlugIn\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InspectResultsAssembly_PlugIn\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSGFDB_PlugIn\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\DMS_Managers\DMS_Update_Manager\bin\DMS_Programs\AnalysisToolManager1\ProteinFileReader.dll" /Y
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_File_Splitter\lib\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_File_Splitter\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_File_Splitter\FastaFileSplitterDLL\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_File_Comparer\lib\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_File_Comparer\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_File_Splitter\lib\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_File_Splitter\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_File_Splitter\FastaFileSplitterDLL\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_File_Comparer\lib\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_File_Comparer\bin\ProteinFileReader.dll" /Y
 pause
 
 @echo off
@@ -17,17 +17,17 @@ echo.
 echo.
 @echo on
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_Organism_Filter\lib\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Fasta_Organism_Filter\bin\Debug\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_Organism_Filter\lib\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Fasta_Organism_Filter\bin\Debug\ProteinFileReader.dll" /Y
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Shared Libraries\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\KenAuberry\Organism_Database_Handler\Release_DLL\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Shared Libraries\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\KenAuberry\Organism_Database_Handler\Release_DLL\ProteinFileReader.dll" /Y
 
-rem Still .NET 2: xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Pep_Aligner\bin\ProteinFileReader.dll" /Y
+rem Still .NET 2: xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Pep_Aligner\bin\ProteinFileReader.dll" /Y
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\PeptideHitResultsProcessor\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\PeptideHitResultsProcessor\PeptideHitResultsProcessor\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\PeptideHitResultsProcessor\PeptideHitResultsProcessor\Lib\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\PeptideHitResultsProcessor\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\PeptideHitResultsProcessor\PeptideHitResultsProcessor\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\PeptideHitResultsProcessor\PeptideHitResultsProcessor\Lib\ProteinFileReader.dll" /Y
 pause
 
 @echo off
@@ -35,15 +35,15 @@ echo.
 echo.
 @echo on
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\Lib" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\bin\x86\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\bin\x64\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\bin\x86\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\bin\x64\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\PeptideToProteinMapEngine\bin\x86\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\PeptideToProteinMapEngine\bin\x64\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\ProteinCoverageSummarizer\bin\x86\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Coverage_Summarizer\ProteinCoverageSummarizer\bin\x64\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\Lib" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\bin\x86\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\bin\x64\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\bin\x86\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\bin\x64\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\PeptideToProteinMapEngine\bin\x86\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\PeptideToProteinMapper\PeptideToProteinMapEngine\bin\x64\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\ProteinCoverageSummarizer\bin\x86\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Coverage_Summarizer\ProteinCoverageSummarizer\bin\x64\ProteinFileReader.dll" /Y
 pause
 
 @echo off
@@ -51,19 +51,19 @@ echo.
 echo.
 @echo on
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Digestion_Simulator\Unused\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\Lib\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Digestion_Simulator\Unused\bin\ProteinFileReader.dll" /Y
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\ProteinNmerGenerator\Lib\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\ProteinNmerGenerator\bin\ProteinFileReader.dll" /Y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\ProteinNmerGenerator\bin\Release\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\ProteinNmerGenerator\Lib\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\ProteinNmerGenerator\bin\ProteinFileReader.dll" /Y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\ProteinNmerGenerator\bin\Release\ProteinFileReader.dll" /Y
 
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\JoshAldrich\AScore\AScore_DLL\lib\" /y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\JoshAldrich\AScore\AScore_Console\bin\Debug" /y
-xcopy ProteinFileReader.dll "F:\My Documents\Projects\JoshAldrich\AScore\AScore_Console\bin\Release" /y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\JoshAldrich\AScore\AScore_DLL\lib\" /y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\JoshAldrich\AScore\AScore_Console\bin\Debug" /y
+xcopy ProteinFileReader.dll "F:\Documents\Projects\JoshAldrich\AScore\AScore_Console\bin\Release" /y
 
-rem xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Protein_Sequence_Motif_Extractor\bin\ProteinFileReader.dll" /Y
-rem Still .NET 2: xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Residue_Frequency_Summarizer\bin\ProteinFileReader.dll" /Y
-rem Still .NET 2: xcopy ProteinFileReader.dll "F:\My Documents\Projects\DataMining\Reverse_Text\bin\ProteinFileReader.dll" /Y
+rem xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Protein_Sequence_Motif_Extractor\bin\ProteinFileReader.dll" /Y
+rem Still .NET 2: xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Residue_Frequency_Summarizer\bin\ProteinFileReader.dll" /Y
+rem Still .NET 2: xcopy ProteinFileReader.dll "F:\Documents\Projects\DataMining\Reverse_Text\bin\ProteinFileReader.dll" /Y
 pause
